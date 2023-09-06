b0a5a8b7212cbc67cff6116f0c22c3982f2fd341
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-09-06 14:18:09 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+some code 
+"""
+
+def mini_subtract(x,y):
+    return x - y 
+
+if __name__ == '__main__':
+    print(mini_subtract(2,1))
---
+++ test_main.py
@@ -0,0 +1,4 @@
+from main import mini_subtract
+
+def test_mini_subtract():
+    assert mini_subtract(2,1) == 1

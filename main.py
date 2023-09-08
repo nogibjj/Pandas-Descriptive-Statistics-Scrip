203c73edfee14a08041532d2855fce0f6bcca34f
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-09-07 23:43:11 -0400

    Update main.py
    changeddddddd

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pandas as pd
 
-data = {"Name": ["Simrun", "Rakeen", "Javier"], "Cooking_Skills": [1000, 300, 5000]}
-print(data)
-df = pd.DataFrame.from_dict(data)
-
-
 ###Descriptive Stats
-average = df["Cooking_Skills"].mean()
-medium = df["Cooking_Skills"].median()
-counts = df["Cooking_Skills"].value_counts()
+def computation (df):
+    average = df["Cooking_Skills"].mean()
+    medium = df["Cooking_Skills"].median()
+    #counts = df["Cooking_Skills"].mode()
+    return (average,medium)
+
 
 if __name__ == "__main__":
-    print(average)
-    print(medium)
-    print(counts)
+    data = {"Name": ["Simrun", "Rakeen", "Javier"], "Cooking_Skills": [1000, 300, 5000]}
+# print(data)
+df = pd.DataFrame.from_dict(data)
+    
+x  = computation (df)
+print (x)

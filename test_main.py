bdcb5976bbc644080532022284767a831dd33f82
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-09-08 00:23:20 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,9 +1,10 @@
 from main import computation
 import pandas as pd
 
-data = {"Name": ["Simrun", "Rakeen", "Javier"], "Cooking_Skills": [1000, 300, 5000]}
-# print(data)
-df = pd.DataFrame.from_dict(data)
+def test_computation():
+    data = {"Name": ["Simrun", "Rakeen", "Javier"], "Cooking_Skills": [1000, 300, 5000]}
+    # print(data)
+    df = pd.DataFrame.from_dict(data)
+        
     
-
-assert computation(df) == (2100.0,1000.0)
+    assert computation(df) == (2100.0,1000.0)

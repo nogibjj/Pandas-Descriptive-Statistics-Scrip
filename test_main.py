3febeea133d02f4ef1cd34edb7e591cf4a4c8cf6
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-09-07 23:55:48 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,3 +1,8 @@
 import main
 
+data = {"Name": ["Simrun", "Rakeen", "Javier"], "Cooking_Skills": [1000, 300, 5000]}
+# print(data)
+df = pd.DataFrame.from_dict(data)
+    
+
 assert computation(df) == (2100.0,1000.0)

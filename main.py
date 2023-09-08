e24a31f9bbf8aeb035adec6b8402fab3fcf95b46
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-09-07 23:21:48 -0400

    Updatedddd main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 data = {"Name": ["Simrun", "Rakeen", "Javier"], "Cooking_Skills": [1000, 300, 5000]}
 print(data)
-df = pd.DataFrame(data.items(), columns=["Name", "Cooking_Skills"])
-df.describe()
-exit()
-# Descriptive Stats
+df = pd.DataFrame.from_dict(data)
+
+
+###Descriptive Stats
 average = df["Cooking_Skills"].mean()
 medium = df["Cooking_Skills"].median()
 counts = df["Cooking_Skills"].value_counts()

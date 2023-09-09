3bcc44462a185ff5a1c435dce6415bdcd53fd198
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-09-09 17:28:29 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 from main import computation
+
 import pandas as pd
 import numpy as np
 import seaborn.objects as sb
@@ -10,9 +11,20 @@ def test_computation():
     "media/nickeubank/MIDS_Data/"
     "master/World_Development_Indicators/wdi_small_tidy_2015.csv")
 
-    test_average = 
+    result = computation(wdi)
+    expected_mean = sum(wdi["Log GDP Per Capita"])/len(wdi["Log GDP Per Capita"])
     
-    #df = pd.DataFrame.from_dict(data)
-        
-    
-    assert computation(df) == (2100.0,1000.0)
+    sort_wdi = sorted(wdi["Log GDP Per Capita"])
+    n = len(sort_wdi)
+    if n % 2 == 0:
+      expected_median = (sort_wdi[n // 2-1] + sort_wdi[n //2]) / 2
+    else:
+      expected_median = sort_wdi[n//2]
+
+    squared_diff_sum = sum((x - expected_mean)**2 for x in wdi["Log GDP Per Capita"])
+    expected_standard_dev = (squared_diff_sum/len(wdi["Log GDP Per Capita"])) ** 0.5
+  
+    assert result == (expected_mean, expected_median, expected_standard_dev)
+
+if __name__ = "__main__":
+    pytest.main()

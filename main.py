ceacc613a92d0908eaffbd37d06fc33b921f3ea3
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-09-09 16:34:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+import pandas as pd
+import numpy as np
+import seaborn.objects as sb
+import seaborn as sns
 
+#Loading Dataset
+wdi = pd.read_csv(
+    "https://media.githubusercontent.com/"
+    "media/nickeubank/MIDS_Data/"
+    "master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
 
+#Taking the log of predictor variable(Gdp Per Capita) to determine relationship with outcome variable(Infant Mortality Rate)
+wdi["Log GDP Per Capita"] = np.log(wdi["GDP per capita (constant 2010 US$)"])
 
+#Visualizing the data representation of the relationship between the two variables. 
+chart = (sb.Plot(wdi, x= "Log GDP Per Capita", y="Mortality rate, under-5 (per 1,000 live births)") 
+.add(sb.Line(), sb.PolyFit(2)) 
+.add(sb.Dot())
+.label(title = "Analysis of Log Gdp Per Capita and Infant Mortality Rate")
+)
+
+#Display of the chart
+chart
+
+#Function created to understand the descriptive statistices for the dataset of WorldBank Indicators
+def computation (dataframe):
+    average = dataframe["Log GDP Per Capita"].mean()
+    medium = dataframe["Log GDP Per Capita"].median()
+    std_dev = dataframe["Log GDP Per Capita"].std()
+    return (average,medium,std_dev)
+
+#Calling the function
+computation(wdi)
 
 
 

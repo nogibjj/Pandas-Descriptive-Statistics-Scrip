427602f7f4ecbeaabc5f479637057c194eb184f4
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-09-09 16:48:54 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,10 +1,18 @@
 from main import computation
 import pandas as pd
+import numpy as np
+import seaborn.objects as sb
+import seaborn as sns
 
 def test_computation():
-    data = {"Name": ["Simrun", "Rakeen", "Javier"], "Cooking_Skills": [1000, 300, 5000]}
-    # print(data)
-    df = pd.DataFrame.from_dict(data)
+    wdi = pd.read_csv(
+    "https://media.githubusercontent.com/"
+    "media/nickeubank/MIDS_Data/"
+    "master/World_Development_Indicators/wdi_small_tidy_2015.csv")
+
+    test_average = 
+    
+    #df = pd.DataFrame.from_dict(data)
         
     
     assert computation(df) == (2100.0,1000.0)

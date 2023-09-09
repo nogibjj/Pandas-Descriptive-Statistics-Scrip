97616c1caac320a7bfe1472cf9c8220e6c123f8c
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-09-09 16:55:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import seaborn.objects as sb
-import seaborn as sns
+
 
 #Loading Dataset
 wdi = pd.read_csv(
@@ -41,21 +41,4 @@ computation(wdi)
 
 
 
-#import pandas as pd
 
-
-###Descriptive Stats
-#def computation (dataframe):
-    #average = dataframe["Cooking_Skills"].mean()
-    #medium = dataframe["Cooking_Skills"].median()
-    ##counts = dataframe["Cooking_Skills"].mode()
-    #return (average,medium)
-
-
-# if __name__ == "__main__":
-#     data = {"Name": ["Simrun", "Rakeen", "Javier"], "Cooking_Skills": [1000, 300, 5000]}
-# # print(data)
-# df = pd.DataFrame.from_dict(data)
-    
-# x  = computation (df)
-# print (x)
